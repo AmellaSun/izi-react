03dbe1e1f1513aa54384da829377f3413fd34ac1
AmellaSun <darja.amelicheva@yandex.ru>
2022-09-24 22:25:38 +0300

    добавлены иконки navbar

--- src/componens/Header.jsx
+++ src/componens/Header.jsx
@@ -3,11 +3,7 @@ import "./Header.css";
 
 const Header = () => {
     return (
-        <header className="header">
-            <img
-                src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg"
-                alt=""
-            />
+        <header>   
         </header>
     );
 };
--- src/componens/Navbar.jsx
+++ src/componens/Navbar.jsx
@@ -13,54 +13,79 @@ import "./Navbar.css";
 const Navbar = () => {
     return (
         <nav className="nav">
-            <div className="nav__boxes">
-                <HomeIcon nameOfClass="nav__icon" />
-                <a href="#" className="nav__a">
-                    Главная
-                </a>
-            </div>
-            <div  className="nav__boxes">
-                <LatticeIcon nameOfClass="nav__icon" />
-                <a href="#" className="nav__a">
-                    Обзор
-                </a>
-            </div>
-            <div  className="nav__boxes">
-                <BellIcon nameOfClass="nav__icon" />
-                <a href="#" className="nav__a">
-                    Уведомления
-                </a>
-            </div>
-            <div  className="nav__boxes">
-                <MessageIcon nameOfClass="nav__icon" />
-                <a href="#" className="nav__a">
-                    Сообщения
-                </a>
-            </div>
-            <div  className="nav__boxes">
-                <BookmarkIcon nameOfClass="nav__icon" />
-                <a href="#" className="nav__a">
-                    Закладки
-                </a>
-            </div>
-            <div  className="nav__boxes">
-                <SkylistsIcon nameOfClass="nav__icon" />
-                <a href="#" className="nav__a">
-                    Списки
-                </a>
-            </div>
-            <div  className="nav__boxes">
-                <ProfileIcon nameOfClass="nav__icon" />
-                <a href="#" className="nav__a">
-                    Профиль
-                </a>
-            </div>
-            <div  className="nav__boxes">
-                <OtherIcon nameOfClass="nav__icon" />
-                <a href="#" className="nav__a">
-                    Ещё
-                </a>
-            </div>
+            <a href="#" className="nav__logo-link">
+                <div className="nav__logo-wrapper">
+                    <img
+                        className="nav__logo"
+                        src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg"
+                        alt=""
+                    />
+                </div>
+            </a>
+            <a href="#" className="nav__link--active" name="1">
+                <div className="nav__boxes">
+                    <HomeIcon nameOfClass="nav__icon" />
+                    <span href="#" className="nav__topic">
+                        Главная
+                    </span>
+                </div>
+            </a>
+            <a href="#" className="nav__link" name="2">
+                <div className="nav__boxes">
+                    <LatticeIcon nameOfClass="nav__icon" />
+                    <span href="#" className="nav__topic">
+                        Обзор
+                    </span>
+                </div>
+            </a>
+            <a href="#" className="nav__link">
+                <div className="nav__boxes">
+                    <BellIcon nameOfClass="nav__icon" />
+                    <span href="#" className="nav__topic">
+                        Уведомления
+                    </span>
+                </div>
+            </a>
+            <a href="#" className="nav__link">
+                <div className="nav__boxes">
+                    <MessageIcon nameOfClass="nav__icon" />
+                    <span href="#" className="nav__topic">
+                        Сообщения
+                    </span>
+                </div>
+            </a>
+            <a href="#" className="nav__link">
+                <div className="nav__boxes">
+                    <BookmarkIcon nameOfClass="nav__icon" />
+                    <span href="#" className="nav__topic">
+                        Закладки
+                    </span>
+                </div>
+            </a>
+            <a href="#" className="nav__link">
+                <div className="nav__boxes">
+                    <SkylistsIcon nameOfClass="nav__icon" />
+                    <span href="#" className="nav__topic">
+                        Списки
+                    </span>
+                </div>
+            </a>
+            <a href="#" className="nav__link">
+                <div className="nav__boxes">
+                    <ProfileIcon nameOfClass="nav__icon" />
+                    <span href="#" className="nav__topic">
+                        Профиль
+                    </span>
+                </div>
+            </a>
+            <a href="#" className="nav__link">
+                <div className="nav__boxes">
+                    <OtherIcon nameOfClass="nav__icon" />
+                    <span href="#" className="nav__topic">
+                        Ещё
+                    </span>
+                </div>
+            </a>
         </nav>
     );
 };
